e7171d0461fe511d511ddbbcaccfa2f09785decc
yaseen <yaseenhdj@gmail.com>
2024-02-04 08:52:45 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/',[DashboardController::class,'index']);
